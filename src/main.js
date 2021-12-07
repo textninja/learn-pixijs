85fe303fad001decb63f8e0d4876ba4ab1ec6a7d
Joe Taylor <joe@textninja.net>
2021-12-07 11:57:13 -0800

    Used method chaining

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ const height = 600;
 const app = new pixi.Application({ antialias: true });
 const graphics = new pixi.Graphics;
 
-graphics.lineStyle(3, 0xffffff, 1);
-graphics.drawRoundedRect(100, 100, width-200, height-200, 20);
+graphics
+    .lineStyle(3, 0xffffff, 1)
+    .drawRoundedRect(100, 100, width-200, height-200, 20);
 
 app.stage.addChild(graphics);
+
 document.body.append(app.view); 
