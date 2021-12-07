4aeef8ef8fb98e81400b9cdc52b10605e018e554
Joe Taylor <joe@textninja.net>
2021-12-07 14:02:10 -0800

    Configured to use retina display

--- src/main.js
+++ src/main.js
@@ -2,21 +2,28 @@ import spriteUrl from './sample.png';
 import './main.css';
 import * as pixi from 'pixi.js';
 
-const width = 800;
-const height = 600;
-const app = new pixi.Application({ antialias: true });
-//const graphics = new pixi.Graphics;
+const width = 500;
+const height = 400;
+const app = new pixi.Application({ width: 500, height: 400, resolution: 2, antialiasing: true, autoDensity: true });
 
+// Sprite 
 let sprite = pixi.Sprite.from(spriteUrl);
+sprite.scale.set(0.5);
 
-app.stage.addChild(sprite);
+const graphics = new pixi.Graphics;
+graphics.beginFill(0x101010);
+graphics.drawRoundedRect(30, 30, width-60, height-60, 20);
 
+
+// Main animation loop
 let elapsed = 0.0;
-
 app.ticker.add(delta => {
     elapsed += delta;
     sprite.x = width / 2 - sprite.width/2 + Math.cos(elapsed/50) * width * 0.4;
+    sprite.y = (height-sprite.height)/2;
 }); 
 
 
+app.stage.addChild(graphics);
+app.stage.addChild(sprite);
 document.body.append(app.view); 
