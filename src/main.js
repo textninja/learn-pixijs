bfe291be727f2f4dc8b8ca9bef0d093485ff1842
Joe Taylor <joe@textninja.net>
2021-12-07 13:33:46 -0800

    Followed first example from pixi docs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
+import spriteUrl from './sample.png';
 import './main.css';
 import * as pixi from 'pixi.js';
 
 const width = 800;
 const height = 600;
 const app = new pixi.Application({ antialias: true });
-const graphics = new pixi.Graphics;
+//const graphics = new pixi.Graphics;
 
-graphics
-    .lineStyle(3, 0xffffff, 1)
-    .drawRoundedRect(100, 100, width-200, height-200, 20);
+let sprite = pixi.Sprite.from(spriteUrl);
 
-app.stage.addChild(graphics);
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+
+app.ticker.add(delta => {
+    elapsed += delta;
+    sprite.x = width / 2 - sprite.width/2 + Math.cos(elapsed/50) * width * 0.4;
+}); 
+
 
 document.body.append(app.view); 
