cb30b8226b13ad3730263365d27757e0ed6852bd
Joe Taylor <joe@textninja.net>
2021-12-07 11:55:50 -0800

    Added a rounded rectangle to graphics area

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import './main.css';
 import * as pixi from 'pixi.js';
 
-const app = new pixi.Application;
+const width = 800;
+const height = 600;
+const app = new pixi.Application({ antialias: true });
+const graphics = new pixi.Graphics;
+
+graphics.lineStyle(3, 0xffffff, 1);
+graphics.drawRoundedRect(100, 100, width-200, height-200, 20);
+
+app.stage.addChild(graphics);
 document.body.append(app.view); 
